640666927d630c5216c5c1fd46b1ebdf483f47aa
Huy Son Pham <pham.huyson87@hotmail.com>
2020-11-03 21:12:06 -0500

    Add "shared" module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedModule } from 'src/app/shared/shared.module';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +16,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    AngularMaterialModule
+  ]
+})
+export class SharedModule { }
