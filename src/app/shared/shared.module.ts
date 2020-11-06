b301b2afdecd83d65b390e62e0128bcd8706c885
Huy Son Pham <pham.huyson87@hotmail.com>
2020-11-05 21:06:06 -0500

    Add "BrowserAnimationsModule" to "SharedModule"

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
 
@@ -11,6 +12,7 @@ import { AngularMaterialModule } from 'src/app/shared/angular-material.module';
     CommonModule
   ],
   exports: [
+    BrowserAnimationsModule,
     AngularMaterialModule
   ]
 })
